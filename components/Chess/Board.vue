1de99612627c85e7b564620dccda10b29213d6c3
Bera Senol <bleuberry2be@icloud.com>
2025-01-07 15:16:52 +0100

    Highlighted moves bugfix

--- components/Chess/Board.vue
+++ components/Chess/Board.vue
@@ -1,52 +1,67 @@
 <template>
-  <div>
-    <h1 class="h-16">{{ selectedSquare }}</h1>
-    <div class="flex flex-col">
-      <div v-for="rank, rankIndex in chess.board()" class="flex">
-        <div v-for="file, fileIndex in rank" class="u-tile-size relative"
-          :key="`${String.fromCharCode(fileIndex + 97)}${8 - rankIndex}`"
-          :class="chess.squareColor(<Square>`${String.fromCharCode(fileIndex + 97)}${8 - rankIndex}`) === 'light' ? 'u-tile-light' : 'u-tile-dark'"
-          :id="`${String.fromCharCode(fileIndex + 97)}${8 - rankIndex}`"
-          @click="onSquareClick(<Square>`${String.fromCharCode(fileIndex + 97)}${8 - rankIndex}`)">
-          <ChessTileLabel :square="`${String.fromCharCode(fileIndex + 97)}${8 - rankIndex}`"
-            :color="chess.squareColor(<Square>`${String.fromCharCode(fileIndex + 97)}${8 - rankIndex}`) === 'light'" />
-          <ChessPiece v-if="file"
-            :type="chess.get(<Square>`${String.fromCharCode(fileIndex + 97)}${8 - rankIndex}`).type"
-            :color="chess.get(<Square>`${String.fromCharCode(fileIndex + 97)}${8 - rankIndex}`).color"
-            :key="`${chess.get(<Square>`${String.fromCharCode(fileIndex + 97)}${8 - rankIndex}`).type}${chess.get(<Square>`${String.fromCharCode(fileIndex + 97)}${8 - rankIndex}`).color}${<Square>`${String.fromCharCode(fileIndex + 97)}${8 - rankIndex}`}`" />
-          <ChessIndicator :id="`${<Square>`${String.fromCharCode(fileIndex + 97)}${8 - rankIndex}`}-indicator`" />
+    <div>
+        <h1 class="h-16">{{ selectedSquare }}</h1>
+        <div class="flex flex-col">
+            <div v-for="rank, rankIndex in chess.board()" class="flex">
+                <div v-for="file, fileIndex in rank" class="u-tile-size relative"
+                    :key="`${String.fromCharCode(fileIndex + 97)}${8 - rankIndex}`"
+                    :class="chess.squareColor(<Square>`${String.fromCharCode(fileIndex + 97)}${8 - rankIndex}`) === 'light' ? 'u-tile-light' : 'u-tile-dark'"
+                    :id="`${String.fromCharCode(fileIndex + 97)}${8 - rankIndex}`"
+                    @click="onSquareClick(<Square>`${String.fromCharCode(fileIndex + 97)}${8 - rankIndex}`)">
+                    <ChessTileLabel :square="`${String.fromCharCode(fileIndex + 97)}${8 - rankIndex}`"
+                        :color="chess.squareColor(<Square>`${String.fromCharCode(fileIndex + 97)}${8 - rankIndex}`) === 'light'" />
+                    <ChessPiece v-if="file"
+                        :type="chess.get(<Square>`${String.fromCharCode(fileIndex + 97)}${8 - rankIndex}`).type"
+                        :color="chess.get(<Square>`${String.fromCharCode(fileIndex + 97)}${8 - rankIndex}`).color"
+                        :key="`${chess.get(<Square>`${String.fromCharCode(fileIndex + 97)}${8 - rankIndex}`).type}${chess.get(<Square>`${String.fromCharCode(fileIndex + 97)}${8 - rankIndex}`).color}${<Square>`${String.fromCharCode(fileIndex + 97)}${8 - rankIndex}`}`" />
+                    <ChessIndicator
+                        :id="`${<Square>`${String.fromCharCode(fileIndex + 97)}${8 - rankIndex}`}-indicator`" />
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 </template>
 
 <script lang="ts" setup>
-import { Chess, type Square } from 'chess.js';
+import { Chess, SQUARES, type Square } from 'chess.js';
 
 const chess = reactive(new Chess());
 const selectedSquare = ref<Square | null>(null);
+const highlightedSquares = ref([]);
 
 const onSquareClick = (square: Square): void => {
-  console.log(chess.get(square))
-  if (chess.get(square)) {
-    // selects square
-    selectedSquare.value = square;
-    console.log(chess.get(<Square>selectedSquare.value))
-  } else if (selectedSquare.value === square && chess.get(square).color === chess.turn()) {
-    // reselects square
-    selectedSquare.value = square;
-    console.log('reselect')
-  } else {
-    try {
-      chess.move({ from: <string>selectedSquare.value, to: square });
-      console.log('move')
-    } catch (e) {
-      console.error(e);
+    removeHighlighedMoves();
+    if (chess.get(square)) {
+        // Select a Square
+        selectedSquare.value = square;
+        highlightMoves(square);
+    } else if (selectedSquare.value === square && chess.get(square).color === chess.turn()) {
+        // Reselect a Square
+        selectedSquare.value = square;
+        // highlightMoves(square);
     }
-    finally {
-      selectedSquare.value = null;
+    else {
+        try {
+            chess.move({ from: <string>selectedSquare.value, to: square });
+
+        } catch (e) {
+            console.error(e);
+        }
+        finally {
+            selectedSquare.value = null;
+        }
     }
-  }
+}
+
+const highlightMoves = (square: Square): void => {
+    for (let i = 0; i < chess.moves({ square: square }).length; i++) {
+        document.getElementById(`${(chess.moves({ square: square, verbose: true })[i].to).toString()}-indicator`)?.classList.add('highlighted')
+    }
+}
+
+const removeHighlighedMoves = (): void => {
+    for (let i = 0; i < SQUARES.length; i++) {
+        document.getElementById(`${SQUARES[i]}-indicator`)?.classList.remove('highlighted');
+    }
 }
 </script>
