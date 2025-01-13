12e9de47a51490e0411fab8c3298caf31dd939a4
Bera Senol <bleuberry2be@icloud.com>
2025-01-13 23:58:31 +0100

    Fen String Tooltips

--- components/Info/Fen.vue
+++ components/Info/Fen.vue
@@ -1,19 +1,7 @@
 <template>
-  <div>
-    <UTooltip class="flex flex-col overflow-auto">
-      <UButton icon="i-heroicons-clipboard-document-check"
-        :variant="useColorMode().value === 'light' ? 'solid' : 'soft'" size="xl"
-        class="flex text-lg tracking-wide justify-center items-center py-6 h-8" :trailing="true"
-        @click="useClipboard().copyToClipboard(<string>fen); useToast().add({ title: 'Succes!', description: 'FEN succesfully copied to clipboard', icon: 'material-symbols:bookmark-check-outline' })">
-        {{ fen }}
-      </UButton>
-    </UTooltip>
-    <div class="flex flex-col items-center pt-6">
-      <p v-for="rank, i in fen?.split(' ')[0].split('/')" class="text-lg tracking-widest">{{ rank }}</p>
-    </div>
-    <div>
-
-    </div>
+  <div style="height: 34rem;">
+    <InfoFenCopyToClipboard :fen="fen" />
+    <InfoFenDeconstructed :fen="fen" />
   </div>
 </template>
 
---
+++ components/Info/Fen/CopyToClipboard.vue
@@ -0,0 +1,17 @@
+<template>
+  <UTooltip class="flex flex-col overflow-auto">
+    <UButton icon="i-heroicons-clipboard-document-check" :variant="useColorMode().value === 'light' ? 'solid' : 'soft'"
+      size="xl" class="flex text-lg tracking-wide justify-center items-center py-6 h-8" :trailing="true"
+      @click="useClipboard().copyToClipboard(<string>fen); useToast().add({ title: 'Succes!', description: 'FEN succesfully copied to clipboard', icon: 'material-symbols:bookmark-check-outline' })">
+      {{ fen }}
+    </UButton>
+  </UTooltip>
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    fen: { type: String, required: false }
+  }
+};
+</script>
